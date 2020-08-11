bcca96da9acc2c44ac5e8597e7c9e58a1b004480
Muhammad Raihan <68261960+mdraihan-r@users.noreply.github.com>
2020-08-11 15:42:37 +0800

    Add files via upload

--- FYP/Controllers/ProgramController.cs
+++ FYP/Controllers/ProgramController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FYPDraft.Controllers
 {
     public class ProgramController : Controller
     {
+        [Authorize(Roles = "Admin, Alumni, Startup")]
         public IActionResult Calendar()
         {
             return View();
